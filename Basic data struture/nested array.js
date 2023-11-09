03d6ff01f75f4d84d3364eba63ce5c943aed7f3e
vipul <rajvipulraj401@gmail.com>
2023-11-09 18:07:36 +0530

    Started objects coding and learning

---
+++ Basic data struture/nested array.js
@@ -0,0 +1,32 @@
+let myNestedArray = [ //this is level 1 outer array 
+
+["level 2"],
+[["level 3", "deep"]],
+[[["deeper" , "level4"]]],
+[[[["deepest" ,"level5"]]]]
+
+  
+];
+
+
+//OR do it like this .
+
+
+let myNestedArray = [
+  // Level 1 array
+  ["dee2"],
+  // Level 2 array
+
+  [["deep"]],
+  // Level 3 array
+  
+  [
+
+    [['d'], ['deeper']],
+        // Level 4 array
+    [
+      // Level 5 array
+      [['deepest'], ["dee"]]
+    ]
+  ]
+];
---
+++ Basic data struture/object.js
@@ -0,0 +1,15 @@
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+foods.grapes = 35;
+foods["bananas"]=13;
+const sweet = "strawberries";
+foods[sweet]=27;
+
+
+console.log(foods);
+
+
+ // OBJECT square and dot notation as well as accessing the //value of a variable using square notation.
