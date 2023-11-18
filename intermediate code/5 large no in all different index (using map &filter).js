481313f11a0c7cb595f8da183cd8999fdb927ca7
vipul <rajvipulraj401@gmail.com>
2023-11-18 10:22:34 +0530

    Added more codes

--- intermediate code/5 large no in all different index (using map &filter).js
+++ intermediate code/5 large no in all different index (using map &filter).js
@@ -1,24 +1,35 @@
 // method 2
+// function largestOfFour(arr) {
+//   return arr.map(function(group) {
+//     // group is the current element which is the sub array in main array
+//     return group.reduce(function(prev, current) {
+//       return current > prev ? current : prev;
+//     },group[0]);
+//   });
+// }
+// const ans =largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// console.log(ans);
+
+// second practise
+
 function largestOfFour(arr) {
-  return arr.map(function(group) {
-    // group is the current element which is the sub array in main array
-    return group.reduce(function(prev, current) {
-      return current > prev ? current : prev;
-    },group[0]);
-  });
+  return arr.map(
+    (curr) =>
+      // we use map method to go at every index and return new array with the maximum of each sub array.
+
+      curr.reduce((acc, current) => (acc > current ? acc : current), curr[0])
+    // we set accumulator to the first index of every sub array which is curr as set in map method.
+    // we use reduce method to find maximum of each subarray.
+  );
+  // return arr;
 }
-const ans =largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+const ans = largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
 
 console.log(ans);
-
-// logic
-      // In this code:
-/* 
-// - We map all items within the main array to a new array using Array.prototype.map().
-// - Within each inner array, we use Array.prototype.reduce() to reduce its contents to a single value.
-// - The callback function passed to the reduce method takes the previous value and the current value, comparing the two.
-// - If the current value is higher than the previous value, it becomes the new previous value for further comparison with the next item within the array.
-// - The result is returned to the map method callback if it's the last item in the inner array.
-// - The final mapped array, containing the maximum values from each inner array, is returned as the result.
-
-*/
